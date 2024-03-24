67ce1b5ed470b19c8e058c631a368c43b46c634c
Karl-Johan Bailey <karl@cenedex.com>
2024-03-24 10:41:06 -0400

    fix: remove migrations

--- prisma/migrations/20240322060954_question_added/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Question` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `tier_level` INTEGER NOT NULL DEFAULT 1,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
