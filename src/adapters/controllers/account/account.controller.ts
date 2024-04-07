abc3c69da27c595ff859ecdd7a8b42e3024e4bbf
Karl-Johan Bailey <karl@cenedex.com>
2024-04-07 17:24:53 -0400

    feat: allow for user creation

--- src/adapters/controllers/account/account.controller.ts
+++ src/adapters/controllers/account/account.controller.ts
@@ -3,9 +3,11 @@ import AccountUseCase from "../../../usecases/account/account.usecase";
 import jwt from 'jsonwebtoken';
 import { SECRETS } from "../../../constants";
 import { UnauthorizedError } from "../../../utils/error";
+import CreateAccountDTO from "../../presenters/dto/account/account.create.dto";
 
 export default {
-    login
+    login,
+    create
 }
 
 function login(usecase: AccountUseCase) {
@@ -18,6 +20,18 @@ function login(usecase: AccountUseCase) {
             if(!result) throw new UnauthorizedError('Incorrect password or username');
             const token = jwt.sign({ sub: result?.id }, secret);
             res.json(token);
+        } catch(err) {
+            next(err)
+        }
+    }
+}
+
+function create(usecase: AccountUseCase) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const data: CreateAccountDTO = req.body;
+        try{
+            const result = await usecase.create(data);
+            res.json(result);
         } catch(err) {
             next(err)
         }
---
+++ src/adapters/presenters/dto/account/account.create.dto.ts
@@ -0,0 +1,8 @@
+interface CreateAccountDTO {
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+}
+
+export default CreateAccountDTO;
